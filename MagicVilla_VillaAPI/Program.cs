b85c7c1755a213eb152398799a7dec0cf349d1d6
Askar Oralkhan <askar4ik123@gmail.com>
2024-01-06 16:46:47 +0600

    chore: added logger

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug() // using serilog logger
+//    .WriteTo.File("log/villalogs.txt", rollingInterval:RollingInterval.Day).CreateLogger();
+
+//builder.Host.UseSerilog(); // start using the serilog logger, download nuget packages
+
 builder.Services.AddControllers().AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
